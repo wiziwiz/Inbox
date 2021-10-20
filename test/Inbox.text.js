baa5d35b232fbcb3988c75858e53e6455f46de4f
wiziwiz <gregory.zu@gmail.com>
2021-10-20 14:48:47 +0000

    test message modification

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -6,17 +6,29 @@ const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
+let initialString = 'Hi there!';
 beforeEach(async ()=>{
     // Get list of accounts
     accounts = await web3.eth.getAccounts();
     // Use one of those accounts to deplys the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments:['Hi there!']})
+        .deploy({data: bytecode, arguments:[initialString]})
         .send({from: accounts[0], gas:'1000000'})
 });
 
 describe ('Inbox', () => {
     it('deploys a contract', ()=>{
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, initialString);
+    });
+
+    it('can change the message', async ()=>{
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 })
