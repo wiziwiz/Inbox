a22c2a219de44ad769187d138ec404200a40d2c8
wiziwiz <gregory.zu@gmail.com>
2021-10-20 12:09:24 +0000

    fix typo in compile.js and start deploy contract tests.

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -2,30 +2,21 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
 
+let accounts;
+let inbox;
+beforeEach(async ()=>{
+    // Get list of accounts
+    accounts = await web3.eth.getAccounts();
+    // Use one of those accounts to deplys the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments:['Hi there!']})
+        .send({from: accounts[0], gas:'1000000'})
+});
 
-// class Car {
-//     park()
-//     {
-//         return 'stop';
-//     }
-
-//     drive () {
-//         return 'vroom';
-//     }
-// }
-
-// let car;
-
-// beforeEach(() => {
-//      car = new Car();
-// })
-
-// describe('Car', () => {
-//     it('park can', () => {
-//         assert.equal(car.park(), 'stop');
-//     });
-//     it('drive can', () => {
-//         assert.equal(car.drive(), 'vroom');
-//     });
-// });
+describe ('Inbox', () => {
+    it('deploys a contract', ()=>{
+        console.log(inbox);
+    });
+})
