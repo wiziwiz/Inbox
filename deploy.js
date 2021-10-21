d772d398371ede70e236afff94407b6734416bdb
wiziwiz <gregory.zu@gmail.com>
2021-10-21 06:58:21 +0000

    deploy to rinkeby test network done

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-const HDWallerProvider = requite('truffle-hdwallet-provider');
+const HDWallerProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
@@ -10,3 +10,13 @@ const provider = new HDWallerProvider(
     rinkeby
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account: ', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+    console.log('Contract deployed to ', result.options.address);
+};
+deploy();
